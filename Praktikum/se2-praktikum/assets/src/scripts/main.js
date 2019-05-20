38b4f916330cf1aed384225c79ee3ed0ef8397da
Benedikt Grether <info@benediktgrether.de>
2019-05-20 13:05:48 +0200

    Add Librarys

--- Praktikum/se2-praktikum/assets/src/scripts/main.js
+++ Praktikum/se2-praktikum/assets/src/scripts/main.js
@@ -6,4 +6,31 @@ $(document).ready(function () {
 
     console.info('DOM Ready');
 
+    $('input, textarea').powerTip({
+        placement: 'e',
+        mouseOnToPopup: true,
+    });
+
+    $('.textareaCounter').on('keyup', function (event) {
+        var count = $(this).val().length;
+        $('.returnCounter').html(count);
+    });
+
+    $('.getCurrentLocation').load(locateMe());
 });
+
+function locateMe() {
+    console.log("test");
+    window.navigator.geolocation.getCurrentPosition(geolocationSuccess);
+}
+function geolocationSuccess(position) {
+    var coords = position.coords;
+    var url = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' +
+        coords.latitude + '&lon=' + coords.longitude;
+    fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        var city = json.address.city;
+        $('.input-text').val(city);
+    });
+}
